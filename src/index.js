0a3eca9b056313dddd89c1e9594d2096f1b8dbeb
Mixail <misha.senc@mail.ru>
2021-10-13 21:43:45 +0300

    added clock layout

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/app/App';
 import reportWebVitals from './reportWebVitals';
+import "./scss/style.scss"
 
 ReactDOM.render(
   <React.StrictMode>
