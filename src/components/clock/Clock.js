96b412aefdc15259b106cb1d6d0259ca93483fa5
Mixail <misha.senc@mail.ru>
2021-10-14 15:33:50 +0300

    for each second, a separate segment has been added on the watch to track user actions

--- src/components/clock/Clock.js
+++ src/components/clock/Clock.js
@@ -1,12 +1,16 @@
-import "./clock.scss"
-import clockSVG from "../../resources/clock.svg"
+import "./clock.scss";
+import clockSVG from "../../resources/clock.svg";
 
 function Clock() {
     const numbers = () => {
         return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((item, i) => {
-            return <div key={i} className={`fractions__item fraction fraction_${i + 1}`}>{12 - i}</div>
-        })
-    }
+            return (
+                <div key={i} className={`fractions__item fraction fraction_${i + 1}`}>
+                    {12 - i}
+                </div>
+            );
+        });
+    };
 
     const hand = (clazz) => {
         return (
@@ -15,19 +19,38 @@ function Clock() {
                     <div className={`hand__item hand__item_${clazz}`} />
                 </div>
             </div>
-        )
-    }
+        );
+    };
+
+    const segments = () => {
+        const segmentList = (count) => {
+            return Array(count)
+                .fill(0)
+                .map((_, i) => {
+                    return (
+                        <li
+                            key={i}
+                            className={`segments__item segments__item_${i}`}
+                            data-count={i}
+                            onMouseEnter={(e) => console.log(`onMouseEnter ${e.target.dataset.count}`)}
+                        />
+                    );
+                });
+        };
+
+        return <ul className="segments">{segmentList(60)}</ul>;
+    };
 
     return (
         <div className="clock">
-            <div className="fractions">
-                {numbers()}
-            </div>
+            <div className="fractions">{numbers()}</div>
             {hand("hour")}
             {hand("minute")}
             {hand("second")}
+            {segments(60)}
             <img src={clockSVG} alt="" className="clock__border" />
-        </div>)
+        </div>
+    );
 }
 
 export default Clock;
